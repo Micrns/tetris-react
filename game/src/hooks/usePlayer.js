793b0c8b4e8e8e64841f979e30f8b3072ec750fc
Oscar Reyes <97998440+Micrns@users.noreply.github.com>
2023-09-27 18:53:28 -0700

    Implementing the players position and controls

--- game/src/hooks/usePlayer.js
+++ game/src/hooks/usePlayer.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import { randomTetromino } from "../tetrominos";
+import { STAGE_HEIGHT, STAGE_WIDTH } from "../gameHelpers";
 
 export const usePlayer = () => {
 
@@ -10,7 +11,25 @@ export const usePlayer = () => {
         collided: false,
     });
 
-    return [player];
+
+
+    const updatePlayerPos = ({x,y, collided}) =>{
+        setPlayer(prev => ({
+            ...prev,
+            pos:{x:(prev.pos.x += x), y:(prev.pos.y += y)},
+            collided,
+        }))
+    }
+
+    const resetPlayer = useCallback(() => {
+        setPlayer({
+            pos:{x:STAGE_WIDTH /2-2, y: 0},
+            tetromino:randomTetromino().shape,
+            collided:false,
+        })
+    }, [])
+
+    return [player, updatePlayerPos, resetPlayer];
 
 
 
